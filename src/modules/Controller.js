28d88d1610c3f5dc306c5b66a330ba7cdce672d7
amaraa <oamraa62@gmail.com>
2022-05-08 16:01:40 +0800

    .

--- src/modules/Controller.js
+++ src/modules/Controller.js
@@ -5,6 +5,7 @@ import {
     TouchableHighlight
   } from 'react-native';
 import { forward, back, right, left, stop } from './ControllerApi'
+import { Joystick } from 'react-joystick-component';
 
 const Controller = () => {
 
@@ -28,30 +29,39 @@ const Controller = () => {
         left()
     }
 
+    const handleMove = () => {
+        console.log()
+    }
+
+    const handleStop = () => {
+        console.log()
+    }
+
     return (
-        <View style={{flex: 1}}>
-            <View>
-                
+        <View style={{flex: 1, flexDirection: 'row'}}>
+            <View style={{flex: 1, paddingLeft: 100, backgroundColor: 'blue'}}>
+
             </View>
             <View style={{ alignItems: 'flex-end', flex: 1, justifyContent: 'space-between' }}>
-                <View style={{ padding: 10, flexDirection: 'row' }}>
+                {() => <Joystick size={100} move={handleMove} stop={handleStop} />}
+                {/* <View style={{ padding: 10, flexDirection: 'row' }}>
                     <TouchableHighlight onLongPress={() => onForward()} onPressOut={() => onCancel()}>
-                        <View style={{backgroundColor: 'blue', height: 70, width: 70}} />
+                        <View style={{backgroundColor: 'blue', height: 60, width: 60}} />
                     </TouchableHighlight>
                     <View style={{ width: 10 }} />
                     <TouchableHighlight onLongPress={() => onBack()} onPressOut={() => onCancel()}>
-                        <View style={{backgroundColor: 'blue', height: 70, width: 70}} />
+                        <View style={{backgroundColor: 'blue', height: 60, width: 60}} />
                     </TouchableHighlight>
                 </View>
                 <View style={{ padding: 20, flex: 1, maxHeight: 190, justifyContent: 'space-between' }}>
                     <TouchableHighlight onLongPress={() => onRight()} onPressOut={() => onCancel()}>
-                        <View style={{backgroundColor: 'blue', height: 70, width: 70}} />
+                        <View style={{backgroundColor: 'blue', height: 60, width: 60}} />
                     </TouchableHighlight>
 
                     <TouchableHighlight onLongPress={() => onLeft()} onPressOut={() => onCancel()}>
-                        <View style={{backgroundColor: 'blue', height: 70, width: 70}} />
+                        <View style={{backgroundColor: 'blue', height: 60, width: 60}} />
                     </TouchableHighlight>
-                </View>
+                </View> */}
             </View>
         </View>
     )
